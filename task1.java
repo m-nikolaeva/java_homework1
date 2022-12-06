c94a5e7bf70731fdb39c7c9ea3cce7410a15d552
Marina <mv.nikolaeva1507@gmail.com>
2022-12-06 17:58:47 +0300

    add homework1

---
+++ task1.java
@@ -0,0 +1,9 @@
+/* Вычислить n-ое треугольного число(сумма чисел от 1 до n), n! (произведение чисел от 1 до n) */
+
+
+public class task1 {
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ task2.java
@@ -0,0 +1,8 @@
+/* Вывести все простые числа от 1 до 1000 */
+
+
+public class task2 {
+    public static void main(String[] args) {
+        
+    }
+}
